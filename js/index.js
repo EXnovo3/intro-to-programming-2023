279782dd3172be2d83eeaf931d7c21ee90fb7fd8
Oxana <144241614+EXnovo3@users.noreply.github.com>
2023-11-15 01:40:13 -0500

    lesson-4-2-FINAL version

--- js/index.js
+++ js/index.js
@@ -1,8 +1,21 @@
 const today = new Date();
 const thisYear = today.getFullYear();
-const footer = document.querySelector('#footer');
+const footer = document.querySelector('footer');
 const copyright = document.createElement('p');
-copyright.innerHTML = "OxanaV"+ thisYear;
+copyright.innerHTML = `Oxana V ${thisYear}`;
+copyright.style.fontSize = "12px";
+copyright.style.textAlign = "center";
 footer.appendChild(copyright);
+
+const skills = ["HTML","CSS","JavaScript","Git","GitHub"];
+const skillsSection = document.getElementById('skills');
+const skillsList = skillsSection.querySelector('ul');
+for (let i = 0; i < skills.length; i++) {
+    let skill = document.createElement('li');
+    skill.innerText = skills[i];
+    skillsList.appendChild(skill);
+}
+
+
 
 
